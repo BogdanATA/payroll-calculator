499a696ba5b0c23780a2470a3d0d56bcdb986061
BogdanATA <batameev@gmail.com>
2026-04-08 15:22:46 -0400

    Created the gather info portion of task

---
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        //gather info
+        System.out.println("Enter employee name: ");
+        String name = myScanner.nextLine();
+
+        System.out.println("Enter hours worked: ");
+        double hoursWorked = myScanner.nextDouble();
+
+        System.out.println("Enter pay rate: ");
+        double payRate = myScanner.nextDouble();
+
+
+
+
+
+    }
+}
